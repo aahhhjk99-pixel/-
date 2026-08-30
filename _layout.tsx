ed6574dc58576b7c9770550a5fb816d2e4b5d345
aahhhjk99-pixel <aahhhjk99@gmail.com>
2026-08-30 21:39:55 +0200

    Update _layout.tsx

--- _layout.tsx
+++ _layout.tsx
@@ -1,12 +1,14 @@
 import { Stack } from 'expo-router';
 
-export default function AuthLayout() {
+export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="login" />
       <Stack.Screen name="customer-signup" />
       <Stack.Screen name="technician-signup" />
+      <Stack.Screen name="users" />
+      <Stack.Screen name="recharges" />
     </Stack>
   );
 }
