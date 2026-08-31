a37def513889095ff583cdfe97dd5a9634a46128
aahhhjk99-pixel <aahhhjk99@gmail.com>
2026-08-31 15:16:42 +0200

    Update _layout.tsx

--- _layout.tsx
+++ _layout.tsx
@@ -1,14 +1,17 @@
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login" />
-      <Stack.Screen name="customer-signup" />
-      <Stack.Screen name="technician-signup" />
-      <Stack.Screen name="users" />
-      <Stack.Screen name="recharges" />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="customer-signup" />
+        <Stack.Screen name="technician-signup" />
+        <Stack.Screen name="users" />
+        <Stack.Screen name="recharges" />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
