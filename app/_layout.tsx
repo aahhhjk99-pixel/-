a25259f10afc2815081f09d3bd57e4229e5bb75b
aahhhjk99-pixel <aahhhjk99@gmail.com>
2026-09-01 01:14:43 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ import { useFonts, Cairo_400Regular, Cairo_500Medium, Cairo_700Bold } from '@exp
 import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from '../lib/theme-context';
 import { AuthProvider } from '../lib/auth';
+import { ToastProvider } from '../lib/toast';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -17,7 +18,6 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    // إخفاء شاشة البداية سواء تم تحميل الخط أو حدث خطأ لعدم تجميد التطبيق
     if (loaded || error) {
       SplashScreen.hideAsync().catch(() => {});
     }
@@ -31,14 +31,16 @@ export default function RootLayout() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AuthProvider>
         <ThemeProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="index" />
-            <Stack.Screen name="login" />
-            <Stack.Screen name="customer-signup" />
-            <Stack.Screen name="technician-signup" />
-            <Stack.Screen name="users" />
-            <Stack.Screen name="recharges" />
-          </Stack>
+          <ToastProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="index" />
+              <Stack.Screen name="login" />
+              <Stack.Screen name="customer-signup" />
+              <Stack.Screen name="technician-signup" />
+              <Stack.Screen name="users" />
+              <Stack.Screen name="recharges" />
+            </Stack>
+          </ToastProvider>
         </ThemeProvider>
       </AuthProvider>
     </GestureHandlerRootView>
